1a9c1333c6d28c09ff5145e566e78dae78a6f119
Kim Takala <kim.takala@helsinki.fi>
2025-05-06 15:56:33 +0300

    fixed import, for compatability

--- src/program.py
+++ src/program.py
@@ -10,7 +10,7 @@ from sys import stdout
 from time import sleep
 
 # Dynamically add the project root directory to sys.path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
